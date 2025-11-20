f20e25fff6dedd97adcd58d8fb19853fe0a4dc12
harshaldhake123 <harshaldhake123@gmail.com>
2025-11-20 23:35:06 +0530

    add shadcn and tailwind setup, also added eslint

--- DocQnA.UI/src/app/app.ts
+++ DocQnA.UI/src/app/app.ts
@@ -1,9 +1,10 @@
+import { UbButtonDirective } from '@/components/ui/button';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UbButtonDirective],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
